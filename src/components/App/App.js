eaace0d7e34dd076f0d1745472d58f57020562d0
Ryan <ryanjmack21@gmail.com>
2018-10-07 08:39:41 -0400

    Add Search to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Pokemon from '../Pokemon/Pokemon';
+import Search from '../Search/Search';
 
 
 class App extends React.Component {
@@ -15,11 +16,14 @@ class App extends React.Component {
     // static data about ALL of the pokemon
     this.data = null;
 
+    this.pokemonSearch = this.pokemonSearch.bind(this);
     this.callAPI = this.callAPI.bind(this);
+    this.callAPI();
+
   }
 
 
-  // get the data about the Pokemon from the API
+  // get the data about the Pokemon from the API and sanitize it a little
   callAPI() {
     const endpoint = 'https://pokeapi.co/api/v2/pokemon/';
 
@@ -28,23 +32,33 @@ class App extends React.Component {
     fetch(req)
       .then(blob => blob.json())
       .then(data => {
-        this.data = data.results.slice(0, 386);
 
+        // 0-386 is pokemon generation I - III.
+        this.data = data.results.slice(0, 386).map((pokemon, i) => {
+           pokemon.id = i + 1;
+           return pokemon;
+        })
+      })
+      .then(() => {
         this.setState({
           results: this.data
         });
-    });
+      });
   }
 
 
-  componentDidMount() {
-    this.callAPI();
+  // handler passed to the the Search component. Can filter the list of pokemon based on user search
+  pokemonSearch(list) {
+    this.setState({
+      results: list
+    });
   }
 
 
   render() {
     return (
       <div className="App">
+        <Search pokemon={this.data} pokemonSearch={this.pokemonSearch} />
         <Pokemon results={this.state.results}/>
       </div>
     )
